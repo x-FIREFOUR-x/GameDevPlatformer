998ce93b1bd0ee41a06df0531f99a34d2922e00d
voonya <nikolaiev.i03@gmail.com>
2023-04-10 09:56:55 +0300

    feat: add StatChangingItemDescriptor

--- Platformer/Assets/Scripts/Items/Data/ItemDescriptor.cs
+++ Platformer/Assets/Scripts/Items/Data/ItemDescriptor.cs
@@ -7,11 +7,11 @@ namespace Items.Data
     [Serializable]
     public class ItemDescriptor
     {
-        public ItemId ItemId { get; private set; }
-        public ItemType Type { get; private set; }
-        public Sprite ItemSprite { get; private set; }
-        public ItemRarity ItemRarity { get; private set; }
-        public float Price { get; private set;  }
+        [field: SerializeField] public ItemId ItemId { get; private set; }
+        [field: SerializeField] public ItemType Type { get; private set; }
+        [field: SerializeField] public Sprite ItemSprite { get; private set; }
+        [field: SerializeField] public ItemRarity ItemRarity { get; private set; }
+        [field: SerializeField] public float Price { get; private set;  }
 
         public ItemDescriptor(ItemId itemId, ItemType type, Sprite itemSprite, ItemRarity itemRarity, float price)
         {
---
+++ Platformer/Assets/Scripts/Items/Data/StatChangingItemDescriptor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Items.Enum;
+using StatsSystem;
+using UnityEngine;
+
+namespace Items.Data
+{
+    [Serializable]
+    public class StatChangingItemDescriptor : ItemDescriptor
+    {
+        [field: SerializeField] public float Level { get; private set; }
+        [field: SerializeField] public List<StatModificator> Stats { get; private set; }
+
+        public StatChangingItemDescriptor(ItemId itemId, ItemType type, Sprite itemSprite, ItemRarity itemRarity, float price, float level, List<StatModificator> stats):
+            base(itemId, type, itemSprite, itemRarity, price)
+        {
+            Level = level;
+            Stats = stats;
+        }
+    }
+}
