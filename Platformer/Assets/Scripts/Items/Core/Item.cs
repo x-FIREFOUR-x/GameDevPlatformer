3d7d3791ba030439ea692d947a893733486314e4
voonya <nikolaiev.i03@gmail.com>
2023-04-10 10:33:35 +0300

    feat: add items classes

---
+++ Platformer/Assets/Scripts/Items/Core/Item.cs
@@ -0,0 +1,12 @@
+﻿using Items.Data;
+
+namespace Items.Core
+{
+    public abstract class Item
+    {
+        public ItemDescriptor Descriptor { get;  }
+        public abstract int Amount { get; }
+        protected Item(ItemDescriptor descriptor) => Descriptor = descriptor;
+        public abstract void Use();
+    }
+}
---
+++ Platformer/Assets/Scripts/Items/Core/Potion.cs
@@ -0,0 +1,47 @@
+﻿using Items.Data;
+using StatsSystem;
+using UnityEngine;
+
+namespace Items.Core
+{
+    public class Potion : Item
+    {
+        private readonly StatsController _statsController;
+        private readonly StatChangingItemDescriptor _itemDescriptor;
+        private int _amount;
+        
+        public override int Amount => -1;
+
+        public Potion(ItemDescriptor descriptor, StatsController statsController) :
+            base(descriptor)
+        {
+            _itemDescriptor = descriptor as StatChangingItemDescriptor;
+            _statsController = statsController;
+            _amount = 1;
+        }
+
+        public override void Use()
+        {
+            _amount--;
+            foreach (var stat in _itemDescriptor.Stats)
+            {
+                _statsController.ProcessModificator(stat);
+            }
+
+            if (_amount <= 0)
+            {
+                Destroy();
+            }
+        }
+
+        private void Destroy()
+        {
+            Debug.Log("Destroy");
+        }
+
+        public void AddToStack(int amount)
+        {
+            _amount += amount;
+        }
+    }
+}
