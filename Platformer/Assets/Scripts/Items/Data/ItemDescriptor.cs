8c439d589b092b3c3ac5e1d78622dcd0b3380301
voonya <nikolaiev.i03@gmail.com>
2023-04-10 09:50:23 +0300

    feat: add ItemType, ItemRarity, ItemId and ItemDescriptor

---
+++ Platformer/Assets/Scripts/Items/Data/ItemDescriptor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Items.Enum;
+using UnityEngine;
+
+namespace Items.Data
+{
+    [Serializable]
+    public class ItemDescriptor
+    {
+        public ItemId ItemId { get; private set; }
+        public ItemType Type { get; private set; }
+        public Sprite ItemSprite { get; private set; }
+        public ItemRarity ItemRarity { get; private set; }
+        public float Price { get; private set;  }
+
+        public ItemDescriptor(ItemId itemId, ItemType type, Sprite itemSprite, ItemRarity itemRarity, float price)
+        {
+            ItemId = itemId;
+            Type = type;
+            ItemSprite = itemSprite;
+            ItemRarity = itemRarity;
+            Price = price;
+        }
+    }
+}
+
---
+++ Platformer/Assets/Scripts/Items/Enum/ItemId.cs
@@ -0,0 +1,10 @@
+﻿namespace Items.Enum
+{
+    public enum ItemId
+    {
+        None = 0,
+        // Change this lines to our items
+        SomeArmor = 1,
+        SomeWeapon = 20,
+    }
+}
