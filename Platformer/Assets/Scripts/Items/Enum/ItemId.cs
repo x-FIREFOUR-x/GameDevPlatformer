75951d74e8d68b0054e6ec5334108535edbfe319
Alex <olexandrpasalsky@gmail.com>
2023-04-12 02:07:01 +0300

    add Items (7 Chests)

--- Platformer/Assets/Scripts/Items/Enum/ItemId.cs
+++ Platformer/Assets/Scripts/Items/Enum/ItemId.cs
@@ -3,8 +3,23 @@
     public enum ItemId
     {
         None = 0,
-        // Change this lines to our items
-        SomeArmor = 1,
-        SomeWeapon = 20,
+
+        //Helmets
+        SomeHelmet = 1,
+
+        //Chests
+        LightChest = 50,
+        ScoutChest = 51,
+        TitanChest = 52,
+        NephritisChest = 53,
+        DarkChest = 54,
+        ShiningChest = 55,
+        DragonChest = 56,
+
+        //Weapons
+        SomeWeapon = 100,
+
+        //Potions
+        SomePotion = 150
     }
 }
