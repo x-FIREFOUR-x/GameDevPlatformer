2b266600b06545d93d8b299453cc1cb38a9a7299
Alex <olexandrpasalsky@gmail.com>
2023-04-12 15:17:43 +0300

    add items (6 Shields)

--- Platformer/Assets/Scripts/Items/Enum/ItemId.cs
+++ Platformer/Assets/Scripts/Items/Enum/ItemId.cs
@@ -14,18 +14,26 @@
         DragonHelmet = 7,
 
         //Chests
-        LightChest = 50,
-        ScoutChest = 51,
-        TitanChest = 52,
-        NephritisChest = 53,
-        DarkChest = 54,
-        ShiningChest = 55,
-        DragonChest = 56,
+        LightChest = 51,
+        ScoutChest = 52,
+        TitanChest = 53,
+        NephritisChest = 54,
+        DarkChest = 55,
+        ShiningChest = 56,
+        DragonChest = 57,
 
         //Weapons
-        SomeWeapon = 100,
+        WoodenShield = 101,
+        IronShield = 102,
+        SteelShield = 103,
+        AmethystShield = 104,
+        MilitiamanShield = 105,
+        VibraniumShield = 106,
+
+        //Weapons
+        SomeWeapon = 150,
 
         //Potions
-        SomePotion = 150
+        SomePotion = 200
     }
 }
