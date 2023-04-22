02e94a6a0aec640e506c088b8d57a02261840fb1
voonya <nikolaiev.i03@gmail.com>
2023-04-22 19:12:08 +0300

    feat: Add ScreenView class

---
+++ Platformer/Assets/Scripts/UI/Core/ScreenView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UI.Core
+{
+    public abstract class ScreenView : MonoBehaviour
+    {
+        [SerializeField] private Canvas _root;
+
+        public virtual void Show() => _root.enabled = true;
+        public virtual void Hide() => _root.enabled = false;
+    }
+}
