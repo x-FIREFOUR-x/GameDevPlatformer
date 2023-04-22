ee18e703771cb050feff41438a34ba1efe0e0b61
Alex <olexandrpasalsky@gmail.com>
2023-04-22 20:31:45 +0300

    feat: create interface IScreenController

---
+++ Platformer/Assets/Scripts/UI/Core/IScreenController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using UI.Enum;
+
+namespace UI.Core
+{
+    public interface IScreenController
+    {
+        public event Action CloseRequested;
+        public event Action<ScreenType> OpenScreenRequested;
+
+        void Initialize();
+
+        void Complete();
+    }
+}
