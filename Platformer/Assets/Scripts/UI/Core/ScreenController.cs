e2b035f7e2cfce08964fab2602d9857a7289cae9
Danil <danilsmy.edu@gmail.com>
2023-04-23 20:29:02 +0300

    Add ScreenController abstract class

---
+++ Platformer/Assets/Scripts/UI/Core/ScreenController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UI.Enum;
+
+namespace UI.Core
+{
+    public abstract class ScreenController<TView> : IScreenController where TView: ScreenView
+    {
+        protected TView View;
+
+        public ScreenController(TView view) => View = view;
+
+        public event Action<ScreenType> OpenScreenRequested;
+        public event Action CloseScreenRequested;
+
+        public virtual void Initialize() => View.Show();
+        public virtual void Complete() => View.Hide();
+
+        protected void RequestClose() => CloseScreenRequested?.Invoke();
+
+        protected void RequestScreen(ScreenType characterScreenType) =>
+            OpenScreenRequested?.Invoke(characterScreenType);
+    }
+}   
