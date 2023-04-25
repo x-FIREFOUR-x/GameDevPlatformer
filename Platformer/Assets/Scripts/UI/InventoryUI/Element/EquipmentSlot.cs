c14dc5aa082ab21ca183a77885ec92057fcd24df
Danil <danilsmy.edu@gmail.com>
2023-04-25 18:22:13 +0300

    Add InventoryScreenPresenter main methods with some implementations, EquipmentSlot

---
+++ Platformer/Assets/Scripts/UI/InventoryUI/Element/EquipmentSlot.cs
@@ -0,0 +1,10 @@
+﻿using Items.Enum;
+using UnityEngine;
+
+namespace UI.InventoryUI.Element
+{
+    public class EquipmentSlot : ItemSlot
+    {
+        [field: SerializeField] public EquipmentType EquipmentType { get; private set; }
+    }
+}
