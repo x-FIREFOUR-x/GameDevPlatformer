d19ef9a71a97a941226a15f875b66487802ae02c
Alex <olexandrpasalsky@gmail.com>
2023-06-07 02:32:38 +0300

    level storage

---
+++ Platformer/Assets/Scripts/LevelSystem/EnemiesData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+using NPC.Enum;
+
+namespace LevelSystem
+{
+    [Serializable]
+    public class EnemiesData
+    {
+        [field: SerializeField] public EntityId TypeEntity { get; private set; }
+        [field: SerializeField] public Vector3 coordinateSpawn { get; private set; }
+    }
+}
---
+++ Platformer/Assets/Scripts/LevelSystem/LevelStorage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelSystem
+{
+    [CreateAssetMenu(fileName = "LevelStorage", menuName = "Level/LevelStorage")]
+    public class LevelStorage: ScriptableObject
+    {
+        [field: SerializeField] public List<EnemiesData> ListEnemiesData { get; private set; }
+    }
+}
