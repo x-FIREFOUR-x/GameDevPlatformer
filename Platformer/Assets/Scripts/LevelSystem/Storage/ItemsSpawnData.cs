18241f0e6323f7f7ce3f1dc95053e460b8d8b157
Alex <olexandrpasalsky@gmail.com>
2023-06-07 04:20:42 +0300

    refactor code

---
+++ Platformer/Assets/Scripts/LevelSystem/Storage/ItemsSpawnData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+using Items.Enum;
+using Items.Scriptable;
+
+namespace LevelSystem.Storage
+{
+    [Serializable]
+    public class ItemsSpawnData
+    {
+        [field: SerializeField] public BaseItemScriptable ItemScriptable { get; private set; }
+        [field: SerializeField] public Vector3 СoordinateSpawn { get; private set; }
+    }
+}
---
+++ Platformer/Assets/Scripts/LevelSystem/Storage/LevelStorage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelSystem.Storage
+{
+    [CreateAssetMenu(fileName = "LevelStorage", menuName = "Level/LevelStorage")]
+    public class LevelStorage: ScriptableObject
+    {
+        [field: SerializeField] public List<EnemiesSpawnData> ListEnemiesSpawnData { get; private set; }
+
+        [field: SerializeField] public List<ItemsSpawnData> ListItemsSpawnData { get; private set; }
+    }
+}
