b1460132a92bac9705430ca0a2a809e79011991c
Alex <olexandrpasalsky@gmail.com>
2023-06-08 00:31:30 +0300

    fix: button switcher

---
+++ Platformer/Assets/Scripts/LevelSystem/SceneSwitchers/FinishSceneSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using LevelSystem;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.LevelSystem.SceneSwitchers
+{
+    class FinishSceneSwitcher : MonoBehaviour
+    {
+        [SerializeField] Button button;
+
+
+        private void Awake()
+        {
+            button.onClick.AddListener(() => MenuScene());
+        }
+
+        public void MenuScene()
+        {
+            SceneController.Instance.MenuGameScene();
+        }
+    }
+}
---
+++ Platformer/Assets/Scripts/LevelSystem/SceneSwitchers/MenuSceneSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using LevelSystem;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.LevelSystem.SceneSwitchers
+{
+    class MenuSceneSwitcher : MonoBehaviour
+    {
+        [SerializeField] Button button;
+
+
+        private void Awake()
+        {
+            button.onClick.AddListener(() => StartGame());
+        }
+
+        public void StartGame()
+        {
+            SceneController.Instance.NextLevelGameScene();
+        }
+    }
+}
